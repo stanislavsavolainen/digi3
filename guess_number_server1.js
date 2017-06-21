cd1f50061678beff140acc63483eedece793255c
stanislav <stanislav.savolainen@mail.com>
2017-06-21 16:57:12 +0300

    random number generator for server

--- guess_number_server1.js
+++ guess_number_server1.js
@@ -1,3 +1,39 @@
+
+var rnd_number = - 1;
+
+//============================================
+
+function random_number(){
+
+    var max_value = 10;
+    var min_value = 1;
+
+    rnd_number = Math.floor(Math.random() * max_value + min_value);
+}
+
+//============================================
+
+function check_number( check_number ){
+
+
+  if(check_number == rnd_number){
+    console.log("Number is equal");
+  }
+
+  else {
+    if( check_number < rnd_number ){ 
+      console.log(" Number is smaller ");
+     }
+    else if(  check_number > rnd_number){ 
+      console.log(" Number is biger ");
+    }
+  }
+
+}
+
+//============================================
+
+
 //nodeJS + express server module "http-server"
 var express = require('express');
 var application = express();
@@ -23,15 +59,22 @@ function request_function1( http_request, http_response){
   console.log("Calculate request happens !");
 
   var  message_for_client = "Server calculate numbers for you .";	
-
+ 
   var obj = http_request.query.data1; //Object -> String array of key-value pairs --> Get  ?value1=123‚ value2=345, value3=abcd 
- http_request.query.
+// http_request.query.
+
+
   
   //you can for-each object of GET values
   //var numbers = obj.value2;
   
   console.log(obj);
-  console.log(http_request.query);
+
+  check_number(obj); //check if number equal or is biger or smaller than
+  
+
+
+  //console.log(http_request.query);
 
   /*
   struct obj{ int value2; }
@@ -49,6 +92,10 @@ application.get('/calculate_numbers', request_function1);
 
 //listern server port + init function
 function  server_init(){
+
+  random_number();
+  console.log("Generate random number :"+rnd_number);
+
 	console.log("Init function , Server begin working at port : " +port);
 }
 //-----
